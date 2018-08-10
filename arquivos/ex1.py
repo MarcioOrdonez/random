058124db9721674ec2d4bc535b6e963095e975a9
Marcio Ordonez <32135812+MarcioOrdonez@users.noreply.github.com>
2018-08-10 16:47:43 -0300

    Add files via upload

--- arquivos/ex1.py
+++ arquivos/ex1.py
@@ -1,7 +1,8 @@
+#encoding: utf-8
 def principal():
     fileCreate()
     x=fileRead()
-    #print(x)
+    formata(x)
 
 def fileCreate():
     file=open('/tmp/usuarios.txt','w')
@@ -10,7 +11,8 @@ anderson        1245698456
 antonio         123456456
 carlos          91257581
 cesar           987458
-rosemary        789456125'''
+rosemary        789456125
+'''
     file.write(usuarios)
     file.close()
 
@@ -19,7 +21,6 @@ def fileRead():
     lista=file.readlines()
     relatorio=[]
     x=[]
-    print(lista)
     for i in range(len(lista)):
         for j in range(len(lista[i])):
             if(lista[i][j]=='\n'):
@@ -27,21 +28,51 @@ def fileRead():
                 x=[]
             else:
                 x.append(lista[i][j])
+    file.close()
     return memoriaUsada(relatorio)
 
 
 
 #funcao quebrada falta arrumar
 def memoriaUsada(relatorio):
+    totalMemoria=0
     for i in range (len(relatorio)):
         x=[]
+        m=[]
+        novo=[]
         x=relatorio[i]
         x=''.join(map(str,x))
-        #','.join(x)
-        print(x)
-        #x[2]=x[2]/1000000
-        #relatorio[i]=x
-        #print(x)
+        m=x[15:-1]
+        m=float(m)
+        m=m/(100000)
+        x=x[0:15]
+        totalMemoria=totalMemoria+m
+        novo.append(x)
+        novo.append(m)
+        relatorio[i]=novo
+    return pctMemoria(totalMemoria,relatorio)
+
+def pctMemoria(totalMemoria,relatorio):
+    for i in range (len(relatorio)):
+        pct=[]
+        pct=(relatorio[i][1]/totalMemoria)*100
+        relatorio[i].append(pct)
+    return relatorio
+
+def formata(x):
+    file=open('/tmp/usuarios.txt','w')
+    formato='''ACME Inc.               Uso do espaço em disco pelos usuários
+------------------------------------------------------------------------
+Nr.  Usuário        Espaço utilizado     % do uso
+'''
+    file.write(formato)
+    for i in range (len(x)):
+        #formato='''{i+1}   {x[i][0]}        {x[i][1]}     %{x[i][2]}
+#'''
+        formato='''{}   {}        {}     %{}
+        '''.format((i),(x[i][0]),(x[i][1]),(x[i][2]))
+        file.write(formato)
+    file.close()
 
 
 principal()
